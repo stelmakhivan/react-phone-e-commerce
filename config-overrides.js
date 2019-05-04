17d173473f45636953bd2c9267c0cf99a88a17aa
Ivan Stelmakh <stelmahivan1994@gmail.com>
2019-05-04 22:16:51 +0300

    added @hol-loader/react-dom

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,9 @@ const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
+  if (env === 'development') {
+    config.resolve.alias['react-dom'] = '@hot-loader/react-dom';
+  }
   config = rewireReactHotLoader(config, env);
   return config;
 };
