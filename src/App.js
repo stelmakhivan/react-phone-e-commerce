cc277a3f45ba892306a5c7c80f65ef751a01419f
Ivan Stelmakh <stelmahivan1994@gmail.com>
2019-05-05 12:06:29 +0300

    finished Navbar component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import styled from 'styled-components';
+
+import logo from '../logo.svg';
+import { ButtonContainer } from './Button';
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <NavWrapper className="navbar navbar-expand-sm  navbar-dark px-sm-5">
+        <Link to="/">
+          <img src={logo} alt="logo" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
+            my cart
+          </ButtonContainer>
+        </Link>
+      </NavWrapper>
+    );
+  }
+}
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`;
