4725c194a549784f412b7250472799c7edc91517
Ivan Stelmakh <stelmahivan1994@gmail.com>
2019-05-07 22:20:13 +0300

    show detail about product

---
+++ src/context.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { storeProducts, detailProduct } from './data';
+
+const ProductContext = React.createContext();
+
+class ProductProvider extends Component {
+  state = {
+    products: [],
+    detailProduct
+  };
+
+  componentDidMount() {
+    this.setProducts();
+  }
+
+  setProducts = () => {
+    let products = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      products = [...products, singleItem];
+    });
+    this.setState(() => {
+      return { products };
+    });
+  };
+
+  getItem = id => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
+  };
+
+  handleDetail = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
+  };
+
+  addToCart = id => {
+    console.log('add to cart id', id);
+  };
+
+  render() {
+    return (
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
+        {this.props.children}
+      </ProductContext.Provider>
+    );
+  }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export { ProductProvider, ProductConsumer };
